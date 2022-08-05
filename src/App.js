282f0d3bc3885a40fb80a13b907998560f0e1014
Parth Dodia <hashtag7532@gmail.com>
2022-08-05 23:16:04 +0530

    Navbar completed
    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Navbar from "./Components/Navbar/Navbar.jsx";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Create from "./Pages/Create";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/create' exact element={<Create />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-
+import "./Navbar.css";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
-    <div className='flex px-10'>
-        <div className='w-1/2'>
-            <button type='submit'>BLOCK START</button>
+    <div className='flex flex-row justify-between bg-[#D4D4D4]  items-center'>
+        <div className=''>
+            <button type='submit' className='ml-[67px] text-[25px] py-[8px]'><Link to="/">BLOCK START</Link></button>
         </div>
-        <div className='w-1/2'>
-            <ul className='list-none flex justify-between items-center '>
-                <li>about</li>
-                <li>create campaign</li>
-                <li>connect wallet</li>
-                <li></li>
+        <div className=''>
+            <ul className='flex items-center'>
+                <li className='px-8'><button className='hover:bg-[#5A5A5A] hover:text-white px-[10px] hover:rounded-[6px] hover:drop-shadow-xl'><Link to="/about">about</Link></button></li>
+                <li className='px-8'><button className='hover:bg-[#5A5A5A] hover:text-white px-[10px] hover:rounded-[6px] hover:drop-shadow-xl'><Link to="/create">create campaign</Link></button></li>
+                <li className='px-8'><button className='hover:bg-[#5A5A5A] hover:text-white px-[10px] hover:rounded-[6px] hover:drop-shadow-xl'>connect wallet</button></li>
+                <li className='px-5'><FontAwesomeIcon icon={faBars} /></li>
             </ul>
         </div>
     </div>
